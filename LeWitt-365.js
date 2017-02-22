13e0f849559ecadec7c58e1e5130db55f04ae5ff
selinalyc <yliu3@cca.edu>
2017-02-22 00:44:32 -0800

    Create LeWitt-365.js

---
+++ LeWitt-365.js
@@ -0,0 +1,17 @@
+function setup(){
+  createCanvas(400,400);
+  background(255);
+  noStroke();
+  fill(200);
+  rect(20,20,180,180);
+  fill(150);
+  rect(20,200,180,180);
+  fill(100);
+  rect(200,20,180,180);
+  fill(50);
+  rect(200,200,180,180);
+}
+
+function draw(){
+
+}
