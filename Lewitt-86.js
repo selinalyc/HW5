ef025e5b4f77b91a63c989bf0c82e1bc31ec678f
selinalyc <yliu3@cca.edu>
2017-02-22 00:32:08 -0800

    Create Lewitt-86.js

---
+++ Lewitt-86.js
@@ -0,0 +1,13 @@
+function setup(){
+  createCanvas(400,400);
+}
+
+function draw(){
+  var x=random(400);
+  var x2=random(400);
+  var y=random(400);
+  var y2=random(400);
+  line(x,y,x2,y2);
+  
+  
+}
