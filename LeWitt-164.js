13ebd3b2a78cb28e284a59c011db569954bea0bb
selinalyc <yliu3@cca.edu>
2017-02-22 00:38:41 -0800

    Create LeWitt-164.js

---
+++ LeWitt-164.js
@@ -0,0 +1,13 @@
+function setup(){
+  createCanvas(400,400);
+  stroke(0);
+  noFill();
+  rect(20,20,360,360);  
+  stroke(250,0,0);
+  line(100,200,300,200);
+  line(150,250,250,150);
+}
+
+function draw(){
+
+}
